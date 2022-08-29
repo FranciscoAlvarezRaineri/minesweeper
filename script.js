9f36b93c422908b02296e4f13d9d32cbc23288b2
Francisco Alvarez Raineri <FranciscoAlvarezRaineri@gmail.com>
2022-08-29 15:02:04 -0300

    fixed downsizing

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+//Checks movile
+mobileCheck = function() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+const isMobile = mobileCheck()
+
 // Variables
 // los elementos del body
 let board = document.getElementById("board");
-
+let titleObj
 
 // las variables de uso
 let arrButtons = []; // Todos los botones, luego los botones sin minas
@@ -13,15 +21,18 @@ let buttonSize = 32;
 //variables relacionadas al estado del tablero
 let firstButtonClicked = false
 
+let xInit;
 let x;
 let y;
 let width;
 
 //variables relacionadas a las minas
-let amountMines; // Cantidad de minas y tamaño del tablero
+let amountMines; // Cantidad de minas
 let minesRatio;
 let mineCount = 0; // guarda cuantas bombas aledañas tiene un boton
-let tempAmountMines = amountMines;
+
+let time = 0;
+let timeX;
 
 
 // Fuciones
@@ -35,9 +46,7 @@ class Title {
 		this.dificulty = document.getElementById("dificulty");
 		this.face = document.getElementById("face");
 		this.instructions = document.getElementById("instructions");
-		this.timer = document.getElementById("timer");
-		this.time = 0;
-		this.timeX;
+		this.timer = document.getElementById("timer");	
 		this.titleGenerate();
 	}
 
@@ -46,66 +55,9 @@ class Title {
 		this.title.style.width = board.style.width
 		this.mines.textContent = amountMines
 		this.face.textContent = "🙂"
-		this.timer.textContent = `${this.time.toString().padStart(4, '0')}`
+		this.timer.textContent = `${time.toString().padStart(4, '0')}`
 		this.titleInit = true
 	}
-	
-	// funciones que manejan el timer
-	timerStart(time) {
-		this.timeX = setInterval(function () {
-			time++
-			this.timer.textContent = `${time.toString().padStart(4, '0')}`}
-		, 1000)
-	}
-	timeStop() {
-	 	clearInterval(this.timeX)
-	}
-
-	// selecciona la dificultad
-	setDificulty(dificultyLevel) {
-		switch (dificultyLevel) {
-			case "easy":
-				x = 16
-				y = 10
-				minesRatio = 5.2
-				break;
-			case "normal":
-				x = 20
-				y = 12
-				minesRatio = 4.8
-				break;
-			case "hard":
-				x = 24
-				y = 14
-				minesRatio = 4.4
-				break;
-			case "custom":
-				titleObj.setCustom()
-				break;
-		}
-		return reset()
-	}
-
-	// funcion que setea el prompt para crear un custom
-	setCustom() {
-		x = window.prompt("Casillas horizontales", 20)
-		y = window.prompt("Casillas verticales", 12)
-		minesRatio = window.prompt("Cantidad de bombas en un ratio. Ej: 4 equivale a 1 mina cada 4 casillas", 4)
-		if (minesRatio <= 2) {
-			alert(`Son demasiadas minas, sería imposible, Elige otros valores.`)
-			return setCustom()
-		}
-		if (minesRatio > 8) {
-			alert(`No son suficientes minas, sería demasiado fácil, Elige otros valores.`)
-			return setCustom()
-		}
-	}
-
-	// alerta sobre las instrucciones
-	showInstructions() {
-		alert("Un click sobre la casilla revela su contenido. El numero revelado indica la cantidad de bombas aledañas a la casilla. Si clickeas en una bomba pierdes. Un click sostenido sobre una casilla sin revelar, la marca como una bomba. Un click sostenido sobre una casilla revelada, revela las aledañas que no esten marcadas como bombas.")
-	}
-
 }
 
 // Crea los casilleros
@@ -181,7 +133,7 @@ class Button {
 // Inicializa el juego cuando se hace click en un cuadrado
 function startGame(button) {
 	firstButtonClicked = true //setea la variable para que no vuelva a correr esta funcion
-	titleObj.timerStart(titleObj.time)
+	timerStart()
 	button.value = "reserved"//evita que al boton clickeado se le asigne una bomba
 	setMines()
 	setValues()
@@ -189,11 +141,13 @@ function startGame(button) {
 
 // Coloca las minas aleatoriamente en el tablero
 function setMines() {
-	for (let i = 0; i < amountMines; i++) {
+	let i = 0;
+	while (i < amountMines) {
 		let location = Math.floor(Math.random()*arrButtons.length)
 		if (arrButtons[location].element.value != "💣" && arrButtons[location].element.value != "reserved") {
 			arrButtons[location].element.value = "💣"
-		}
+			i++
+		}		
 	}
 }
 
@@ -311,28 +265,43 @@ function endGameReveal(button) {
 
 // Inicializa el tablero, creando los botones 
 function boardGenerate() {
-	arrButtons = []
 	board.innerHTML = ''
 	firstButtonClicked = false	
+	x = checkSize()
 	board.style.columnCount = x
 	board.style.width = `${(x*buttonSize)}px`
 	amountMines = Math.floor(x * y / minesRatio)
-	tempAmountMines = amountMines
+	arrButtons = []
 	for (let i = 0; i < y; i++) {
 		for (let j = 0; j < x; j++) {
 			new Button (i, j);
 		}
 	}
-	checkSize()
 }
 
 //lo hace "responsive"
 function checkSize() {
-	width = x*(buttonSize+1)
-	if(width > window.innerWidth || width > screen.width) {
-		x--
-		return reset()
+	width = x * (buttonSize + 1)
+	if (!(isMobile)) {
+		if(width > window.innerWidth) {
+			x--
+			checkSize()
+		}
+		if((width + buttonSize) < window.innerWidth && x < xInit) {
+			x++
+			checkSize()
+		}
+	} else if(isMobile) {
+		if(width > screen.width) {
+			x--
+			checkSize()
+		}
+		if((width + 2*buttonSize) < screen.width) {
+			x++
+			checkSize()
+		}
 	}
+	return x
 }
 
 
@@ -354,24 +323,82 @@ function endWon() {
 
 // reset general del juego
 function reset(){
-	titleObj.timeStop()
 	boardGenerate()
 	titleObj = {}
 	titleObj = new Title
+	timeStop()
+}
+
+// funciones que manejan el timer
+function timerStart() {
+	timeX = setInterval(function () {
+		time++
+		titleObj.timer.textContent = `${time.toString().padStart(4, '0')}`}
+	, 1000)
+}
+function timeStop() {
+ 	clearInterval(timeX)
+}
+
+// funcion que setea el prompt para crear un custom
+function setCustom() {
+	x = window.prompt("Casillas horizontales", 20)
+	y = window.prompt("Casillas verticales", 12)
+	minesRatio = window.prompt("Cantidad de bombas en un ratio. Ej: 4 equivale a 1 mina cada 4 casillas", 4)
+	if (minesRatio <= 2) {
+		alert(`Son demasiadas minas, sería imposible, Elige otros valores.`)
+		return setCustom()
+	}
+	if (minesRatio > 8) {
+		alert(`No son suficientes minas, sería demasiado fácil, Elige otros valores.`)
+		return setCustom()
+	}
+}
+
+// alerta sobre las instrucciones
+function showInstructions() {
+	alert("Un click sobre la casilla revela su contenido. El numero revelado indica la cantidad de bombas aledañas a la casilla. Si clickeas en una bomba pierdes. Un click sostenido sobre una casilla sin revelar, la marca como una bomba. Un click sostenido sobre una casilla revelada, revela las aledañas que no esten marcadas como bombas.")
+}
+
+// selecciona la dificultad
+function setDificulty(dificultyLevel) {
+	switch (dificultyLevel) {
+		case "easy":
+			xInit = 16
+			y = 10
+			minesRatio = 5.2
+			break;
+		case "normal":
+			xInit = 20
+			y = 12
+			minesRatio = 4.8
+			break;
+		case "hard":
+			xInit = 24
+			y = 14
+			minesRatio = 4.4
+			break;
+		case "custom":
+			titleObj.setCustom()
+			break;
+	}
+	x = xInit
+}
+
+function setEvents() {
+	document.getElementById("easy").addEventListener("click", function() {setDificulty("easy"); return reset()})
+	document.getElementById("normal").addEventListener("click", function() {setDificulty("normal"); return reset()})
+	document.getElementById("hard").addEventListener("click", function() {setDificulty("hard"); return reset()})
+	document.getElementById("custom").addEventListener("click", function() {setDificulty("custom"); return reset()})
+	titleObj.face.addEventListener("click", function() {reset()})
+	titleObj.instructions.addEventListener("click", function() {titleObj.showInstructions()})
+	window.onresize = function() {reset()}
 }
 
 
 // Ejecución
 
-x = 20
-y = 12
-minesRatio = 4.8
+setDificulty("normal")
 boardGenerate()
-let titleObj = new Title
-document.getElementById("easy").addEventListener("click", function() {titleObj.setDificulty("easy")})
-document.getElementById("normal").addEventListener("click", function() {titleObj.setDificulty("normal")})
-document.getElementById("hard").addEventListener("click", function() {titleObj.setDificulty("hard")})
-document.getElementById("custom").addEventListener("click", function() {titleObj.setDificulty("custom")})
-titleObj.face.addEventListener("click", function() {reset()})
-titleObj.instructions.addEventListener("click", function() {titleObj.showInstructions()})
-window.onresize = function() {checkSize()}
+titleObj = new Title
+setEvents()
